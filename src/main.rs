bf2caf6d22dcd4c9ebc0bfd489011d2c51939c29
Michael Ducharm <ducharmdev@gmail.com>
2021-12-01 16:26:28 -0500

    support specifying day as cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
+use std::env;
+
 mod days;
 mod helper;
 
 fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    
     let input_data = include_input_data!("day1");
 
-    days::day1::run(&input_data);
+    if let Some(arg) = args.get(1) {
+
+        let day_to_run = match arg.as_str() {
+            "day1" | "1" => days::day1::run,
+            _ => do_nothing
+        };
+
+        day_to_run(&input_data);
+    } else {
+        // Run all days
+        days::day1::run(&input_data);
+    }
 
 
     Ok(())
+}
+
+fn do_nothing(_x: &[(&str, &str)]) {
+    println!("not a valid argument");
 }
